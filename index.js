2ad53e1edaa0b1d8e671da63d19bd98c5c96e55d
veryfreebirdy <dotuchimlatui@gmail.com>
2020-08-26 16:44:45 +0700

    add usenativedriver

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { StyleSheet, StatusBar, Dimensions, View, Animated, Easing } from 'react-native';
-import NativeLinearGradient from 'react-native-linear-gradient';
-import rgb2hex from 'rgb2hex';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  StatusBar,
+  Dimensions,
+  View,
+  Animated,
+  Easing,
+} from "react-native";
+import NativeLinearGradient from "react-native-linear-gradient";
+import rgb2hex from "rgb2hex";
 
 // const {height, width} = Dimensions.get('window');
 
@@ -17,83 +24,81 @@ class LinearGradient extends Component {
         colors={[color0, color1].map((c) => rgb2hex(c).hex)}
         start={gStart}
         end={gEnd}
-        style={[styles.linearGradient]}>
+        style={[styles.linearGradient]}
+      >
         {children}
       </NativeLinearGradient>
-    )
+    );
   }
 }
-Animated.LinearGradient = Animated.createAnimatedComponent(LinearGradient)
-Animated.useNativeDriver = true
+Animated.LinearGradient = Animated.createAnimatedComponent(LinearGradient);
+Animated.useNativeDriver = true;
 // Animated.NativeLinearGradient = Animated.createAnimatedComponent(NativeLinearGradient)
 
 export const presetColors = {
   instagram: [
-    'rgb(106, 57, 171)',
-    'rgb(151, 52, 160)',
-    'rgb(197, 57, 92)',
-    'rgb(231, 166, 73)',
-    'rgb(181, 70, 92)'
+    "rgb(106, 57, 171)",
+    "rgb(151, 52, 160)",
+    "rgb(197, 57, 92)",
+    "rgb(231, 166, 73)",
+    "rgb(181, 70, 92)",
   ],
   firefox: [
-    'rgb(236, 190, 55)',
-    'rgb(215, 110, 51)',
-    'rgb(181, 63, 49)',
-    'rgb(192, 71, 45)',
+    "rgb(236, 190, 55)",
+    "rgb(215, 110, 51)",
+    "rgb(181, 63, 49)",
+    "rgb(192, 71, 45)",
   ],
   sunrise: [
-    'rgb(92, 160, 186)',
-    'rgb(106, 166, 186)',
-    'rgb(142, 191, 186)',
-    'rgb(172, 211, 186)',
-    'rgb(239, 235, 186)',
-    'rgb(212, 222, 206)',
-    'rgb(187, 216, 200)',
-    'rgb(152, 197, 190)',
-    'rgb(100, 173, 186)',
-  ]
+    "rgb(92, 160, 186)",
+    "rgb(106, 166, 186)",
+    "rgb(142, 191, 186)",
+    "rgb(172, 211, 186)",
+    "rgb(239, 235, 186)",
+    "rgb(212, 222, 206)",
+    "rgb(187, 216, 200)",
+    "rgb(152, 197, 190)",
+    "rgb(100, 173, 186)",
+  ],
 };
 
 class AnimatedGradient extends Component {
-
   static defaultProps = {
     customColors: presetColors.instagram,
     speed: 4000,
     points: {
       start: { x: 0, y: 0.4 },
-      end: { x: 1, y: 0.6 }
-    }
-  }
+      end: { x: 1, y: 0.6 },
+    },
+  };
 
   state = {
     color0: new Animated.Value(0),
     color1: new Animated.Value(0),
-  }
+  };
 
   componentDidMount = () => {
     this.startAnimation();
-  }
+  };
 
   startAnimation = () => {
     const { color0, color1 } = this.state;
     const { customColors, speed } = this.props;
-    [color0, color1].forEach(color => color.setValue(0));
+    [color0, color1].forEach((color) => color.setValue(0));
 
     Animated.parallel(
-      [color0, color1].map(animatedColor => {
+      [color0, color1].map((animatedColor) => {
         return Animated.timing(animatedColor, {
           toValue: customColors.length,
           duration: customColors.length * speed,
-          easing: Easing.linear
-        })
+          easing: Easing.linear,
+          useNativeDriver: true,
+        });
       })
-    )
-      .start(this.startAnimation);
-
+    ).start(this.startAnimation);
   };
 
   render() {
-
     const { color0, color1 } = this.state;
     const { customColors, children, points, style } = this.props;
     const preferColors = [];
@@ -103,13 +108,16 @@ class AnimatedGradient extends Component {
         customColors
           .slice(preferColors.length)
           .concat(customColors.slice(0, preferColors.length + 1))
-      )
+      );
     }
     const interpolatedColors = [color0, color1].map((animatedColor, index) => {
       return animatedColor.interpolate({
-        inputRange: Array.from({ length: customColors.length + 1 }, (v, k) => k),
-        outputRange: preferColors[index]
-      })
+        inputRange: Array.from(
+          { length: customColors.length + 1 },
+          (v, k) => k
+        ),
+        outputRange: preferColors[index],
+      });
     });
 
     return (
@@ -117,26 +125,25 @@ class AnimatedGradient extends Component {
         style={[styles.linearGradient, style]}
         points={points}
         color0={interpolatedColors[0]}
-        color1={interpolatedColors[1]}>
+        color1={interpolatedColors[1]}
+      >
         {children}
       </Animated.LinearGradient>
-    )
+    );
   }
 }
 
-
 const styles = StyleSheet.create({
   linearGradient: {
-    position: 'absolute',
+    position: "absolute",
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'stretch',
+    flexDirection: "column",
+    alignItems: "stretch",
     left: 0,
     right: 0,
     top: 0,
-    bottom: 0
-  }
+    bottom: 0,
+  },
 });
 
-
-export default AnimatedGradient
+export default AnimatedGradient;
